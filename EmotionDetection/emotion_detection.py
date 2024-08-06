841481d464bb4b0da036767425de5781d701cc4b
Mark Cohen <mark.cohen13@gmail.com>
2024-08-06 17:39:47 -0400

    Task 5 completed

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,16 @@
+import requests
+import json as js
+
+URL = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+HEADERS = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+
+def emotion_detector(text_to_analyze):
+    req = requests.post(url=URL, headers=HEADERS, json={"raw_document": {"text": text_to_analyze}})
+    json_dict = js.loads(req.text)
+    emotions = json_dict["emotionPredictions"][0]["emotion"]
+    dominant_emotions = [k for k,v in emotions.items() if v==max(list(emotions.values()))]
+    emotions["dominant_emotion"] = ",".join(dominant_emotions)
+    return emotions
+
+if __name__ == '__main__':
+    print(emotion_detector("I love this new technology"))
