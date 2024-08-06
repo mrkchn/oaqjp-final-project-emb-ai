1c6ff7f5fc3be92a13dcf8ce49b0c68efb30172f
Mark Cohen <mark.cohen13@gmail.com>
2024-08-06 18:13:04 -0400

    Task 8 completed.

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -5,11 +5,17 @@ URL = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/Nl
 HEADERS = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
 
 def emotion_detector(text_to_analyze):
+    """
+    This function evaluates the emotional tenor of the input text
+    """
     req = requests.post(url=URL, headers=HEADERS, json={"raw_document": {"text": text_to_analyze}})
-    json_dict = js.loads(req.text)
-    emotions = json_dict["emotionPredictions"][0]["emotion"]
-    dominant_emotions = [k for k,v in emotions.items() if v==max(list(emotions.values()))]
-    emotions["dominant_emotion"] = ",".join(dominant_emotions)
+    if req.status_code == 200:
+        json_dict = js.loads(req.text)
+        emotions = json_dict["emotionPredictions"][0]["emotion"]
+        dominant_emotions = [k for k,v in emotions.items() if v==max(list(emotions.values()))]
+        emotions["dominant_emotion"] = ",".join(dominant_emotions)
+    elif req.status_code == 400:
+        emotions = {x:None for x in ["anger", "disgust", "fear", "joy", "sadness", "dominant_emotion"]}
     return emotions
 
 if __name__ == '__main__':
--- server.py
+++ server.py
@@ -1,3 +1,7 @@
+"""
+This module uses flask to create a webapp.
+"""
+
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,17 +9,27 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def emot_detector():
+    """
+    This route evaluates the emotional tenor of the input text.
+    """
     # Retrieve the text to analyze from the request arguments
     text_to_analyze = request.args.get('textToAnalyze')
     # Pass the text to the sentiment_analyzer function and store the response
     response = emotion_detector(text_to_analyze)
-    dominant_emotion = f"The dominant emotion is {response.pop('dominant_emotion')}"
-    return f"For the given statement, the system response is {', '.join(k+': '+str(v) for k,v in response.items())}. {dominant_emotion}. "
+    if response["dominant_emotion"]:
+        dominant_emotion = f"The dominant emotion is {response.pop('dominant_emotion')}"
+        text_output = ', '.join(k+': '+str(v) for k,v in response.items())
+        text = f"For the given statement, the system response is {text_output}. {dominant_emotion}."
+    else:
+        text = "Invalid text! Please try again!"
+    return text
 
 @app.route("/")
 def render_index_page():
+    """
+    This route renders the template.
+    """
     return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-
